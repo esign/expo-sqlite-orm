62a85675e647db71773b9220d7bc4a6fd37c3f92
Dieter Vyncke <dietervyncke@Dieters-MacBook-Pro-2.local>
2023-11-21 17:35:15 +0100

    Add: Or operator

--- src/query_builder/read.ts
+++ src/query_builder/read.ts
@@ -22,84 +22,113 @@ export function find(tableName: string) {
  * })
  */
 export function query<T = {}>(tableName: string, options: IQueryOptions<T> = {}) {
-  const { columns, page, limit, where, order } = {
-    ...defaultOptions,
-    ...options
-  }
-
-  const whereStatement = queryWhere(where)
-  let sqlParts = [
-    'SELECT',
-    columns.join(', '),
-    'FROM',
-    tableName,
-    whereStatement,
-    'ORDER BY',
-    Object.entries(order).map(p => p.join(' ')).join(', ')
-  ]
-
-  if (limit !== null) {
-    sqlParts.push(...[
-      'LIMIT',
-      `${limit}`,
-    ])
-    if (page !== null) {
-      sqlParts.push(...[
-        'OFFSET',
-        `${limit * (page - 1)}`
-      ])
+    const {columns, page, limit, where, order} = {
+        ...defaultOptions,
+        ...options
     }
-  }
 
-  return sqlParts.filter(p => p !== '').join(' ')
+    const whereStatement = queryWhere(where)
+    let sqlParts = [
+        'SELECT',
+        columns.join(', '),
+        'FROM',
+        tableName,
+        whereStatement,
+        'ORDER BY',
+        Object.entries(order).map(p => p.join(' ')).join(', ')
+    ]
+
+    if (limit !== null) {
+        sqlParts.push(...[
+            'LIMIT',
+            `${limit}`,
+        ])
+        if (page !== null) {
+            sqlParts.push(...[
+                'OFFSET',
+                `${limit * (page - 1)}`
+            ])
+        }
+    }
+
+    return sqlParts.filter(p => p !== '').join(' ')
 }
 
 function getParameterStatement(option: IQueryOperation, value: string | number | any[]) {
-  if (Array.isArray(value) && ['in', 'notIn'].includes(option)) {
-    return `(${'?'.repeat(value.length).split('').join(', ')})`
-  }
+    if (Array.isArray(value) && ['in', 'notIn'].includes(option)) {
+        return `(${'?'.repeat(value.length).split('').join(', ')})`
+    }
 
-  return '?'
+    return '?'
 }
 
 
 // Convert operators to database syntax
 export function propertyOperation<T extends {}>(property: keyof T, options: Partial<Record<IQueryOperation, any>>) {
-  const operations: Record<IQueryOperation, string> = {
-    equals: '=',
-    notEquals: '<>',
-    lt: '<',
-    lte: '<=',
-    gt: '>',
-    gte: '>=',
-    contains: 'LIKE',
-    in: 'IN',
-    notIn: 'NOT IN'
-  }
-
-  return Object.keys(options).map((option: IQueryOperation) => {
-    if (!operations[option]) {
-      throw new Error(
-        `Operation not found, use (${Object.keys(operations).join(', ')})`
-      )
+    const operations: Record<IQueryOperation, string> = {
+        equals: '=',
+        notEquals: '<>',
+        lt: '<',
+        lte: '<=',
+        gt: '>',
+        gte: '>=',
+        contains: 'LIKE',
+        in: 'IN',
+        notIn: 'NOT IN'
     }
-    return `${String(property)} ${operations[option]} ${getParameterStatement(option, options[option])}`
-  }).join(' AND ')
+
+    return Object.keys(options)
+        .filter(option => option !== 'operator')
+        .map((option) => {
+            if (!operations[option]) {
+                throw new Error(`Operation not found, use (${Object.keys(operations).join(', ')})`);
+            }
+            return `${String(property)} ${operations[option]} ${getParameterStatement(option, options[option])}`;
+        }).join(' AND ');
+}
+
+function buildWhereQuery(options) {
+    let operator = 'AND';
+
+    if (Array.isArray(options)) {
+        const list = options.map((option, key) => {
+            const {query, operator} = buildWhereQuery(option);
+
+            if (key === options.length - 1) {
+                return query;
+            }
+
+            return query + ' ' + operator;
+        });
+
+        return {query: list.join(' ')};
+    }
+
+    const list = Object.entries(options)
+        .map(([property, conditions]) => {
+            // Ignore empty or undefined fields
+            conditions = Object.keys(conditions).reduce((acc, key) => {
+                if (key === 'operator') {
+                    operator = conditions[key];
+                }
+
+                if (conditions[key] !== undefined)
+                    acc[key] = conditions[key];
+                return acc;
+            }, {});
+            return `${propertyOperation(property, conditions)}`;
+        }).filter((item) => Object.values(item).length > 0);
+
+    return {
+        query: list.length > 0 ? list.join(` ${operator} `) : '',
+        operator: operator
+    };
 }
 
 // Build where query
-export function queryWhere<T = any>(options: IQueryWhere<T>) {
-  const list = Object.entries(options).map(([property, conditions]) => {
-    
-    // Ignore empty or undefined fields
-    conditions = Object.keys(conditions).reduce((acc, key) => {
-      if (conditions[key] !== undefined) acc[key] = conditions[key]
-      return acc
-    }, {})
-    
-    return `${propertyOperation(property, conditions)}`
-  }).filter((item) => Object.values(item).length > 0)
-  return list.length > 0 ? `WHERE ${list.join(' AND ')}` : ''
+export function queryWhere(options) {
+    const {query} = buildWhereQuery(options);
+    return query ? `WHERE ${query}` : '';
 }
 
-export default { find, query }
+export default {find, query}
